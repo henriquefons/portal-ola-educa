fa9a73371e67f4ec18126bdbd9c7fb9fb65c52a5
Henrique <henriquefons@hotmail.com>
2021-01-17 19:46:56 -0300

    terminado o header responsive e navbar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,54 +1,52 @@
 import React from 'react';
+import './style.css';
 import { Link } from 'react-router-dom';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const elems = document.querySelectorAll('.sidenav');
-  M.Sidenav.init(elems, {});
-});
+import { Navbar, Icon } from 'react-materialize';
 
 const NavBar = () => (
-  <>
-    <div className="navbar-fixed">
-      <nav>
-        <div className="nav-wrapper nav-bar__index">
-          <Link to="/" className="brand-logo">
-            Olá Educa
-          </Link>
-          <Link to="nav-mobile" className="sidenav-trigger">
-            <i className="material-icons">menu</i>
-          </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <Link to="/">FERRAMENTAS</Link>
-            </li>
-            <li>
-              <Link to="/">METODOLOGIAS</Link>
-            </li>
-            <li>
-              <Link to="/">DICAS</Link>
-            </li>
-            <li>
-              <Link to="/">SOBRE</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-    <ul className="sidenav" id="mobile-demo">
-      <li>
-        <Link to="/">FERRAMENTAS</Link>
-      </li>
-      <li>
-        <Link to="/">METODOLOGIAS ATIVAS</Link>
-      </li>
-      <li>
-        <Link to="/">DICAS</Link>
-      </li>
-      <li>
-        <Link to="/">SOBRE</Link>
-      </li>
-    </ul>
-  </>
+  <Navbar
+    className="components-navbar"
+    fixed="true"
+    alignLinks="right"
+    brand={
+      <Link className="brand-logo" to="/">
+        Olá Educa
+      </Link>
+    }
+    id="mobile-nav"
+    menuIcon={<Icon>menu</Icon>}
+    options={{
+      draggable: true,
+      edge: 'left',
+      inDuration: 250,
+      onCloseEnd: null,
+      onCloseStart: null,
+      onOpenEnd: null,
+      onOpenStart: null,
+      outDuration: 200,
+      preventScrolling: true,
+    }}
+    sidenav={
+      <ul>
+        <li>
+          <Link to="/">FERRAMENTAS</Link>
+        </li>
+        <li>
+          <Link to="/">METODOLOGIAS</Link>
+        </li>
+        <li>
+          <Link to="/">DICAS</Link>
+        </li>
+        <li>
+          <Link to="/">SOBRE</Link>
+        </li>
+      </ul>
+    }
+  >
+    <Link to="/">FERRAMENTAS</Link>
+    <Link to="/">METODOLOGIAS</Link>
+    <Link to="/">DICAS</Link>
+    <Link to="/">SOBRE</Link>
+  </Navbar>
 );
-
 export default NavBar;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Header from '../../components/Header/header';
 import NavBar from '../../components/NavBar/navbar';
 
-const Home = () => <NavBar />;
+const Home = () => (
+  <>
+    <NavBar />
+    <Header
+      title="DESCUBRA NOVAS TECNOLOGIAS E REVOLUCIONE AS TURMAS"
+      desc="Explore novas ferramentas tecnológicas digitais e turbine os resultados das metodologias de aprendizagem ativas"
+    />
+  </>
+);
 
 export default Home;
